2d6ae74dc6e725164849369e3b622d050edd65cd
Vitor Pontual <vitorpguedes@me.com>
2020-10-13 21:45:14 -0300

    Dia 2 Finalizado[Backend]

---
+++ aulas/backend/src/database/migrations/1602625254181-create_images.ts
@@ -0,0 +1,42 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createImages1602625254181 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+       await queryRunner.createTable(new Table({
+	  name: 'images',
+	  columns: [
+	     {
+		name: 'id',
+		type: 'integer',
+		unsigned: true,
+		isPrimary: true,
+		generationStrategy: 'increment'
+	     },
+	     {
+		name: 'path',
+		type: 'varchar'
+	     },
+	     {
+	     name: 'orphanage_id',
+	     type: 'intenger'
+	     },
+	  ],
+	  foreignKeys: [
+	     {
+		name: 'ImageOrphanage',
+		columnNames: ['orphanage_id'],
+		referencedTableName: 'orphanages',
+		referencedColumnNames: ['id'],
+		onUpdate: 'CASCADE',
+		onDelete: 'CASCADE',
+	     }
+	  ],
+       }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+       await queryRunner.dropTable('images')
+    }
+
+}
